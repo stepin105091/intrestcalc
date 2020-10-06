15ba1707c9c03edd7c491562b3013708548c9c1c
stepin105091 <72447355+stepin105091@users.noreply.github.com>
2020-10-06 22:29:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,47 +2,86 @@
 
 int main()
 {
-int choice,fibonacci_limit,factorial_input,factorial_output,prime_input,prime_output;
-printf("From the below option choose the mathematical operation you need to perform\n");
-printf("1.Fibonacci series\n");
-printf("2.Find factorial of the number\n");
-printf("3.Check whether the number is prime or not.\n");
-printf("\n Enter your choice - ");
-scanf("%d",&choice);
-switch(choice)
-    {
-        case 1:
-            printf("\n----------------------\n");
-            printf("\nEnter fibonacci limit\n");
-            scanf("%d",&fibonacci_limit);
-            fibonacci_func(fibonacci_limit);
-            break;
+	
+double principle,rate,time,simple_intrest;
 
-        case 2:
-            printf("\n----------------------\n");
-            printf("\nEnter the number to know factorial\n");
-            scanf("%d",&factorial_input);
-            factorial_output = factorial_func(factorial_input);
-            printf("The factorial is %d",factorial_output);
-            break;
+printf("enter principle");
+printf("\n");
 
-        case 3:
-            printf("\n----------------------\n");
-            printf("\nEnter the number to know whether its prime or not.\n");
-            scanf("%d",&prime_input);
-            prime_output = prime_func(prime_input);
-            if(prime_output==1)
-            {
-                printf("\nIt's a prime number\n");
-            }
-            else
-            {
-                printf("\nIt's not a prime number");
-            }
-            break;
-        default:
-            printf("\nPlease choose the correct option\n");
-            break;
-    }
+scanf("%lf",&principle);
 
+printf("enter rate of intrest");
+printf("\n");
+
+scanf("%lf",&rate);
+
+printf("enter time in months");
+printf("\n");
+
+scanf("%lf",&time);
+
+//principle value greater than zero
+
+if(principle<=0)
+{
+	printf("enter enter the value greater than zero");
+	
+	exit(0);
+}
+
+//rate value greater than zero
+
+if(rate<=0)
+{
+	printf("enter enter the value greater than zero");
+	
+	exit(0);
+}
+
+//time value greater than zero
+
+if(time<=0)
+{
+	printf("enter enter the value greater than zero");
+	
+	exit(0);
+}
+
+printf("enter 1 for simple intrest");
+
+printf("\n");
+
+printf("enter 2 for compound intrest");
+
+printf("\n");
+
+int enter;
+
+scanf("%d",&enter);
+
+if(enter==1)
+{
+	
+	double SI=FindSimple(principle,rate,time);
+	
+	printf("%lf",SI);
+	
+}
+
+if(enter==2)
+{
+
+double CI=FindCompound(principle,rate,time);
+	
+	printf("%lf",CI);
+}
+
+if(enter>2)
+{
+	printf("please choose the correct value try again");
+	
+	exit(0);
+	
+}
+return 0;
 }
